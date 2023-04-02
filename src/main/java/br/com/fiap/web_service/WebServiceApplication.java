2f7727ffa472e04bcbb305ef264abfe241bf4961
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-04-01 23:08:00 -0300

    refactor: main maethod

--- src/main/java/br/com/fiap/web_service/WebServiceApplication.java
+++ src/main/java/br/com/fiap/web_service/WebServiceApplication.java
@@ -1,20 +1,15 @@
 package br.com.fiap.web_service;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @ComponentScan(basePackages = { "br.com.fiap", "br.com.fiap.config" })
 public class WebServiceApplication {
 
 	public static void main(String[] args) {
-		String port = System.getenv("PORT");
-		if (port == null || port.isEmpty()) {
-			port = "8080"; // default port
-		}
-		SpringApplication app = new SpringApplication(WebServiceApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", port));
-		app.run(args);
+		SpringApplication.run(WebServiceApplication.class, args);
 	}
+
 }
